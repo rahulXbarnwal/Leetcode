d0ed559bc645abee5edd9d2ad9acb81a2a4f2641
Rahul Barnwal <78689930+rahulXbarnwal@users.noreply.github.com>
2022-04-15 18:34:14 +0530

    Time: 20ms (61.23%), Space: 23.9 MB (74.78%)

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(root==NULL)
-            return NULL;
+            return root;
         if(root->val <low) {
             return trimBST(root->right, low, high);
         }
